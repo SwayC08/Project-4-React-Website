f8d7f9338124ae569008a1805d63692fd61424b6
“Josue <ijosue.carreon@gmail.com>
2023-02-21 14:24:18 -0600

    update 2pm

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,21 +7,21 @@ const Portfolio = () => {
             <section class="first-project">
                 <h3>1<sup>st</sup> Project: Website Development</h3>
                 <img src="https://dynamicxperts.com/wp-saadali/uploads/2022/05/Web-Designers-Web-Design-1.jpg" alt="Website construction picture"/>
-                <p class="indent">I created a personal website to showcase my skills and display my personal projects. Purus sit amet luctus venenatis lectus magna. Elementum facilisis leo vel fringilla est ullamcorper eget nulla. Pellentesque id nibh tortor id aliquet lectus proin nibh. Egestas dui id ornare arcu. Mauris ultrices eros in cursus turpis massa tincidunt dui.</p>
-                <p class="indent"> Euismod quis viverra nibh cras pulvinar mattis nunc. Dictum sit amet justo donec enim diam vulputate ut pharetra. Eget duis at tellus at urna. Iaculis eu non diam phasellus vestibulum lorem sed risus ultricies. Egestas egestas fringilla phasellus faucibus.</p>
+                <p class="indent">I created a personal website, using only HTML & CSS, to showcase my skills and display my personal projects.</p>
+                <p class="indent"> Here is a link to the site: <a href="https://storied-mooncake-2c670a.netlify.app/index.html" alt="Personal Site Link" target="_blank">My Personal Site</a></p>
             </section>
             <section class="second-project">
-                <h3>2<sup>nd</sup> Project: Application Development </h3>
+                <h3>2<sup>nd</sup> Project: Cookie Clicker Game </h3>
                 <img src="https://www.spinutech.com/webres/Image/web-design-development/articles/Web%20Dev-Blog.png" alt="Application construction picture"/>
-                <p class="indent"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                <p class="indent"> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <p class="indent">  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ut faucibus pulvinar elementum integer enim neque volutpat ac tincidunt. Euismod quis viverra nibh cras pulvinar mattis nunc.</p>
+                <p class="indent"> I created a cookie clicker game using JavaScript functions and logic.</p>
+                <p class="indent"> Here is a link to the site: <a href="https://serene-lebkuchen-a632ea.netlify.app" alt="Cookie Clicker Game Link" target="_blank">Cookie Clicker Game</a></p>
+                
             </section>
             <section class="third-project">
-                <h3>3<sup>rd</sup> Project: Software Development</h3>
+                <h3>3<sup>rd</sup> Project: TicTacToe Game</h3>
                 <img src="https://www.impactbnd.com/hs-fs/hubfs/development-terms-featurd.jpg?length=1200&name=development-terms-featurd.jpg" alt="Software construction picture"/>
-                <p class="indent">Dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo. Parturient montes nascetur ridiculus mus. Eget est lorem ipsum dolor sit amet. Ac turpis egestas integer eget aliquet nibh praesent tristique. Feugiat vivamus at augue eget arcu dictum. Nisi lacus sed viverra tellus in hac habitasse platea dictumst. Ut faucibus pulvinar elementum integer enim neque volutpat ac tincidunt. </p>
-                <p class="indent"> Amet nisl suscipit adipiscing bibendum est. Nunc non blandit massa enim nec dui nunc. Non diam phasellus vestibulum lorem sed risus ultricies tristique nulla. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Risus viverra adipiscing at in tellus integer. Ornare aenean euismod elementum nisi quis eleifend quam.</p>
+                <p class="indent"> I created a TicTacToe game using JavaScript</p>
+                <p class="indent"> Here is a link to the site: <a href="https://rad-snickerdoodle-6254c2.netlify.app/" alt="TicTacToe Game Link" target="_blank">TicTacToe Game</a></p>
             </section>
         </main>
     )
